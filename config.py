f223647575eff24d50fd99fd572c7ef81767515a
fan777 <fan777@gmail.com>
2021-05-30 18:12:37 -0700

    fix postgres --> postgresql in config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 '''Flask config'''
 import os
+import re
 
 ############################################
 # General
@@ -8,6 +9,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "yKwxyju9bYpbWcwTQiFotjgMLpoIcoM-rMmk4
 ############################################
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///travel')
+if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
